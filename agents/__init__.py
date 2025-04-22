f8c3780b03e55efd2d651369a7cf701246dcfb87
wms2537 <weimengsoh0827@gmail.com>
2025-04-23 02:50:01 +0800

    initial commit

---
+++ agents/__init__.py
@@ -0,0 +1,23 @@
+# Initializes the agents module
+
+from .coordinator import CoordinatorAgent
+from .specialist import SpecialistAgent
+from .decision_risk import DecisionRiskAgent
+from .problem_solving import ProblemSolvingInnovationAgent
+from .systems_complexity import SystemsComplexityAgent
+from .bias_psychology import BiasPsychologyAgent
+from .strategy_competition import StrategyCompetitionAgent
+from .learning_communication import LearningCommunicationAgent
+from .efficiency_process import EfficiencyProcessAgent
+
+__all__ = [
+    "CoordinatorAgent",
+    "SpecialistAgent",
+    "DecisionRiskAgent",
+    "ProblemSolvingInnovationAgent",
+    "SystemsComplexityAgent",
+    "BiasPsychologyAgent",
+    "StrategyCompetitionAgent",
+    "LearningCommunicationAgent",
+    "EfficiencyProcessAgent",
+]
